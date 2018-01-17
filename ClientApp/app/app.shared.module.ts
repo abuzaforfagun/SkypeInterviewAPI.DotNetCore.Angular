941697cf73ddb68dfdbded2153fd7fd5292dbcc4
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2018-01-17 18:00:08 +0600

    Move interview in components folder

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -9,7 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
-import { InterviewComponent } from './interview/interview.component';
+import { InterviewComponent } from './components/interview/interview.component';
 
 @NgModule({
     declarations: [
